30852b8fb0bcd5a665317f8ae559b7a25d4766fa
Eli Fuchsman <104859844+efuchsman@users.noreply.github.com>
2022-11-04 21:06:55 -0600

    Revert "Bk feature/merchant us5"

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  # admin
-  get '/admin', to: 'admin#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/admin", to: "admin#index"
   # merchants
   get '/merchants/:merchant_id/dashboard', to: 'merchants#show'
 
   resources :merchants, only: [] do
     resources :items, only: [:index]
     resources :invoices, only: [:index]
-    resources :invoices, only: [:show], controller: 'merchant_invoices'
   end
 end
